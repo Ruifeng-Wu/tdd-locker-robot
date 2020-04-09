0ce3bcba6a738b53da25cf3b0f3fa97714ef04f9
tw26467 <wei.wei@thoughtworks.com>
2020-04-09 21:07:22 +0800

    [feat]when_save_given_robot_has_two_lockers_with_capacity_of_0_2

--- src/test/java/cn/xpbootcamp/gilded_rose/SmartRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SmartRobotTest.java
@@ -23,4 +23,14 @@ public class SmartRobotTest {
 
         Assertions.assertThrows(NoCapacityException.class, () -> smartRobot.save(aPackage));
     }
+
+    @Test
+    void should_return_ticket_when_save_given_robot_has_two_lockers_with_capacity_of_0_2() {
+        SmartRobot smartRobot = new SmartRobot(0, 2);
+        Package aPackage = new Package();
+
+        Ticket ticket = smartRobot.save(aPackage);
+
+        Assertions.assertNotNull(ticket);
+    }
 }
